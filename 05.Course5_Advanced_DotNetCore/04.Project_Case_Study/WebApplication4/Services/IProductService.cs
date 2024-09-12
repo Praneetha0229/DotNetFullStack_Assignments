321c8d36c260ae0df581202829230d4f63b5f71b
Devulapalli Praneetha <94820249+Praneetha0229@users.noreply.github.com>
2024-09-12 15:13:39 +0530

    Add files via upload

---
+++ 05.Course5_Advanced_DotNetCore/04.Project_Case_Study/WebApplication4/Services/IProductService.cs
@@ -0,0 +1,17 @@
+﻿using WebApplication4.Models;
+
+namespace WebApplication4.Services
+{
+    public interface IProductService
+    {
+        List<Product> GetAllProducts();
+        void CreateProduct(Product obj);
+        Product GetProductById(int id);
+        List<Product> GetProductsByCategory(string category);
+        List<Product> GetOutOfStockProducts();
+        List<Product> GetProductsBetweenPriceRange(int lowerPriceLimit, int upperPriceLimit);
+        List<string> GetProductsCategoryNames();
+        void EditProduct(Product obj);
+        string DeleteProductById(int id);
+    }
+}
